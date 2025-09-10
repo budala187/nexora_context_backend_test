3bfed465c2c26cd44b3f300faaa8aae5b40cc6cb
budala187 <kojatobudalapucanamene@gmail.com>
2025-09-10 20:36:35 +0200

    message edited

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,9 +7,6 @@ export const config = {
   serverVersion: '1.0.0',
   port: Number(process.env.PORT), // Render will always provide this
   baseUrl: process.env.BASE_URL || '', // Must be set in environment variables
-  skEnvUrl: process.env.SK_ENV_URL || '',
-  skClientId: process.env.SK_CLIENT_ID || '',
-  skClientSecret: process.env.SK_CLIENT_SECRET || '',
   logLevel: 'info',
   mcpServerId: process.env.MCP_SERVER_ID || '',
   clerkIssuer: 'https://honest-cod-68.clerk.accounts.dev',
