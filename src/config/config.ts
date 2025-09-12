ad7f7fa73149d6a26e07c8acc94ade61dfe8df1e
budala187 <kojatobudalapucanamene@gmail.com>
2025-09-12 17:28:46 +0200

    update

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,9 +7,9 @@ export const config = {
   serverVersion: '1.0.0',
   port: Number(process.env.PORT), // Render will always provide this
   baseUrl: process.env.BASE_URL || '', // Must be set in environment variables
-  skEnvUrl: process.env.SK_ENV_URL || '',
-  skClientId: process.env.SK_CLIENT_ID || '',
-  skClientSecret: process.env.SK_CLIENT_SECRET || '',
+  skEnvUrl: process.env.SCALEKIT_ENVIRONMENT_URL,
+  skClientId: process.env.SCALEKIT_CLIENT_ID,
+  skClientSecret: process.env.SCALEKIT_CLIENT_SECRET,
   logLevel: 'info',
   mcpServerId: process.env.MCP_SERVER_ID || '',
   protectedResourceMetadata: process.env.PROTECTED_RESOURCE_METADATA || '',
